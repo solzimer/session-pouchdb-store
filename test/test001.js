7fee6b9f2c2333fd55f8fa14d2f5dc88cffda34c
solzimer <solzimer@gmail.com>
2018-01-21 16:55:47 +0100

    tests

---
+++ test/test001.js
@@ -0,0 +1,27 @@
+const
+	PouchStore = require("../"),
+	assert = require('assert');
+
+const
+	SID1 = "12345678",
+	SID2 = "abcdefgh";
+
+describe('Basic features', function() {
+	var store = new PouchStore();
+	var session1 = {data : {key1:"val1", key2:"val2"}}
+
+	describe('#set(sid, session, cb)', function() {
+		it('Should store a new session', function() {
+			store.set(SID1, session1, assert.ifError);
+		});
+	});
+
+	describe('#get(sid, cb)', function() {
+		it('Should retrieve the created session (cached)', function() {
+			store.get(SID1, function(err, sess){
+				assert.deepEqual(sess.data,session1.data);
+			});
+		});
+	});
+
+});
